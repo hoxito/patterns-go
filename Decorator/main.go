3642a5fb4084786fcba208144bc3aa9cfea68d0f
hoxito <60928120+hoxito@users.noreply.github.com>
2022-07-07 22:14:42 -0300

    it counts, idk what it counts

--- Decorator/main.go
+++ Decorator/main.go
@@ -9,13 +9,14 @@ import (
 func ProfileDecorator[T, R any](a func(T) R) func(T) R {
 
 	defer func(t time.Time) {
-		fmt.Printf("--- Time Elapsed: %v ---n", time.Since(t))
+		fmt.Printf("--- Time Elapsed 1: %v ---n", time.Since(t))
 	}(time.Now())
 	return func(params T) R {
-		start := time.Now()
+		defer func(t time.Time) {
+			fmt.Printf("--- Time Elapsed 2: %v ---n", time.Since(t))
+		}(time.Now())
+		time.Sleep(1000)
 		result := a(params)
-		elapsed := time.Now().Sub(start)
-		fmt.Println("Time Elapsed: ", elapsed)
 		return result
 	}
 }
