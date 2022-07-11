f204b8c0589a8168b072d180b052e139b7ee2cee
hoxito <60928120+hoxito@users.noreply.github.com>
2022-07-11 20:51:57 -0300

    readme structure

--- Decorator/main.go
+++ Decorator/main.go
@@ -7,14 +7,10 @@ import (
 
 func ProfileDecorator[T, R any](a func(T) R) func(T) R {
 
-	defer func(t time.Time) {
-		fmt.Printf("--- Time Elapsed 1: %v ---n", time.Since(t))
-	}(time.Now())
 	return func(params T) R {
 		defer func(t time.Time) {
 			fmt.Printf("--- Time Elapsed 2: %v ---n", time.Since(t))
 		}(time.Now())
-		time.Sleep(1000)
 		result := a(params)
 		return result
 	}
