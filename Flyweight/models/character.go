ac95b805895334e7f65fd6f63588ae5c6db45e34
jose Aranciba <josearanciba09@gmail.com>
2022-07-26 09:30:26 -0300

    flyweight

--- Flyweight/models/character.go
+++ Flyweight/models/character.go
@@ -1,49 +1,82 @@
-package characters
+package models
 
 type iCharacter interface {
-	setName(Name string)
 	setAttackDamage(damage int)
 	setAttackDistance(distance int)
 	setWeapon(Weapon string)
 	getAttackDistance() int
 	getWeapon() string
-	getName() string
 }
 
-type character struct {
-	Name           string `json:"name"`
+type mage struct {
 	AttackDamage   int    `json:"attackDamage"`
 	AttackDistance int    `json:"attackDistance"`
 	Weapon         string `json:"weapon"`
 }
 
-func (c *character) setName(Name string) {
-	c.Name = Name
-}
-
-func (c *character) getName() string {
-	return c.Name
-}
-
-func (c *character) setWeapon(Weapon string) {
+func (c *mage) setWeapon(Weapon string) {
 	c.Weapon = Weapon
 }
 
-func (c *character) getWeapon() string {
+func (c *mage) getWeapon() string {
 	return c.Weapon
 }
-func (c *character) setAttackDamage(damage int) {
+func (c *mage) setAttackDamage(damage int) {
 	c.AttackDamage = damage
 }
 
-func (c *character) getAttackDamage() int {
+func (c *mage) getAttackDamage() int {
 	return c.AttackDamage
 }
 
-func (c *character) setAttackDistance(damage int) {
+func (c *mage) setAttackDistance(damage int) {
 	c.AttackDistance = damage
 }
 
-func (c *character) getAttackDistance() int {
+func (c *mage) getAttackDistance() int {
 	return c.AttackDistance
+}
+
+func newMage() iCharacter {
+	return &mage{
+		AttackDamage:   80,
+		AttackDistance: 200,
+		Weapon:         "Staff",
+	}
+}
+
+type warrior struct {
+	AttackDamage   int    `json:"attackDamage"`
+	AttackDistance int    `json:"attackDistance"`
+	Weapon         string `json:"weapon"`
+}
+
+func (c *warrior) setWeapon(Weapon string) {
+	c.Weapon = Weapon
+}
+
+func (c *warrior) getWeapon() string {
+	return c.Weapon
+}
+func (c *warrior) setAttackDamage(damage int) {
+	c.AttackDamage = damage
+}
+
+func (c *warrior) getAttackDamage() int {
+	return c.AttackDamage
+}
+
+func (c *warrior) setAttackDistance(damage int) {
+	c.AttackDistance = damage
+}
+
+func (c *warrior) getAttackDistance() int {
+	return c.AttackDistance
+}
+func newWarrior() iCharacter {
+	return &warrior{
+		AttackDamage:   150,
+		AttackDistance: 50,
+		Weapon:         "sword",
+	}
 }
--- Flyweight/models/characterTypeFactory.go
+++ Flyweight/models/characterTypeFactory.go
@@ -8,27 +8,27 @@ const (
 )
 
 var (
-	characterTypeFactorySingleton = &characterTypeFactory{
-		characterTypeMap: make(map[string]CharacterType),
+	characterTypeFactorySingleton = &CharacterTypeFactory{
+		characterTypeMap: make(map[string]iCharacter),
 	}
 )
 
-type characterTypeFactory struct {
-	characterTypeMap map[string]CharacterType
+type CharacterTypeFactory struct {
+	characterTypeMap map[string]iCharacter
 }
 
-func (d *CharacterTypeFactory) getDressByType(CharacterType string) (Character, error) {
-	if d.dressMap[CharacterType] != nil {
-		return d.dressMap[CharacterType], nil
+func (d *CharacterTypeFactory) getDressByType(CharacterType string) (iCharacter, error) {
+	if d.characterTypeMap[CharacterType] != nil {
+		return d.characterTypeMap[CharacterType], nil
 	}
 
 	if CharacterType == mageType {
-		d.dressMap[CharacterType] = newMage()
-		return d.dressMap[CharacterType], nil
+		d.characterTypeMap[CharacterType] = newMage()
+		return d.characterTypeMap[CharacterType], nil
 	}
 	if CharacterType == warriorType {
-		d.dressMap[CharacterType] = newWarrior()
-		return d.dressMap[CharacterType], nil
+		d.characterTypeMap[CharacterType] = newWarrior()
+		return d.characterTypeMap[CharacterType], nil
 	}
 
 	return nil, fmt.Errorf("Wrong character type passed")
